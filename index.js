6eb4208fbda0e428a5d37542414325588d84c68e
Manjeet Panghal <manjeet13804@gmail..com>
2022-11-30 19:13:43 +0530

    file updated successfully

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const app =  express()
 app.use("/api/user",userRoutes)
 app.use("/api/property",propertyRoutes)
 
-
+app.use(cors({
+    origin:"*"
+}))
 
 app.listen(5000, () => console.log("The server is up at 5000 port"));
