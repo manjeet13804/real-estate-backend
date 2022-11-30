de0f3c3d7bd3ec3b33ac72802cfe920c32d1ee8e
Manjeet Panghal <manjeet13804@gmail..com>
2022-11-30 11:55:30 +0530

    file updated successfully

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const userRoutes = require("./routes/user");
+const propertyRoutes=require("./routes/property")
 const connect = require("./connection/connect");
 var jwt = require('jsonwebtoken');
 const secret = "ESTATE"
--- models/property.js
+++ models/property.js
@@ -2,16 +2,51 @@ const mongoose = require('mongoose');
  const Schema = mongoose.Schema;
 
  const addPropertySchema = new Schema({
-    ppdId: {type:String,unique}, 
-    image: String,
-    property: [String], 
-    contact: {type:Number},
-    area: Number, 
-    views: {type: Number},
-    status: {type: String, default: "Unsold"},
-    daysLeft: {type:Number}
+   propertyType: { required: true, type: String },
+   negotiable: { type: String },
+   price: { type: String },
+   ownership: { type: String },
+   propertyAge: { type: String },
+   propertyApproved: { type: String },
+   propertyAge: { type: String },
+   propertyDescription: { type: String },
+   bankLoan: { type: String },
+   length: { required: true, type: String },
+   breath: { required: true, type: String },
+   totalArea: { required: true, type: String },
+   areaUnit: { type: String },
+   noOfBHK: { type: String },
+   noOfFloor: { type: String },
+   attached: { type: String },
+   western: { type: String },
+   furnished: { type: String },
+   carParking: { type: String },
+   lift: { type: String },
+   electricity: { type: String },
+   facing: { type: String },
 
+   name: { type: String },
+   contact: { required: true, type: String },
+   postedBy: { type: String },
+   saleType: { type: String },
+   featuredPackage: { type: String },
+   ppdPackage: { type: String },
+
+   email: { required: true, type: String },
+   city: { type: String },
+   area: { type: String },
+   pincode: { type: String },
+   address: { type: String },
+   landmark: { type: String },
+   latitude: { type: String },
+   longitude: { type: String },
+   PPDId: { required: true, type: String },
+   Views: { required: true, type: String },
+   DaysLeft: { required: true, type: String },
    
+   image: String,
+   status: {type: String, default: "Unsold"},
+   users : {type :mongoose.Schema.Types.ObjectId, ref: "Users"}
  })
 const addPropertyModel = mongoose.model("Property", addPropertySchema);
 module.exports = addPropertyModel;
