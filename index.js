7fe833c6241a2a17f01ddb5c28c17fe9c899ca0b
manjeet13804 <51025738+manjeet13804@users.noreply.github.com>
2022-12-24 16:49:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
 const express = require("express");
 const mongoose = require('mongoose');
+mongoose.connect(
+  "mongodb+srv://lalatendu_14:Liku2324@cluster0.cb2danw.mongodb.net/realestateproject?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("successfully connected to db");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
 const userRoutes = require("./routes/user");
 const propertyRoutes=require("./routes/property")
 // const assetsRoutes = require("./routes/assets")
@@ -14,16 +25,6 @@ const app =  express()
 app.use("/api/user",userRoutes)
 app.use("/api/property",propertyRoutes)
 // app.use("/api/property",assetsRoutes)
-mongoose.connect(
-  "mongodb+srv://lalatendu_14:Liku2324@cluster0.cb2danw.mongodb.net/realestateproject?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("successfully connected to db");
-  },
-  (err) => {
-    console.log(err);
-  }
-);
 app.use(cors({
     origin:"*"
 }))
